2b999111c10ba4d4fab5734ae7829753a62b5977
kom-1010 <kom-1010@naver.com>
2020-06-14 17:54:04 +0900

    chat template 적용

--- group-control-service/config/static/js/websock.js
+++ group-control-service/config/static/js/websock.js
@@ -8,7 +8,37 @@ chatSocket.onmessage = function (e) {
   var data = JSON.parse(e.data);
   var author = data["author"];
   var message = data["message"];
-  document.querySelector("#chat-log").value += author + " : " + message + "\n";
+  if (user === author) {
+  document.querySelector("#chat-log").innerHTML += `
+    <div style="width: 100%; display: inline-block;">
+        <div class="col-4 mt-3 mb-1" style="background-color: rgba(0, 0, 0, 0); float: right;">
+            <div class="card" style="background-color: rgba(0, 0, 0, 0); border-color: rgba(0, 0, 0, 0);">
+                <div style="padding-top: 1.25rem; background-image: url(${myChatTopUrl}); background-size: 100%, 100%; background-repeat: no-repeat;">
+                </div>
+          <div style="flex: 1 1 auto; background-image: url(${myChatMidUrl}); background-size: 100%, 100%; padding: 0 1.25rem 1rem 1.25rem;">
+            <h5 class="card-title">${author}</h5>
+            ${message}
+          </div>
+          <div class="card-body" style="background-image: url(${myChatBotUrl});background-size: 100%, 100%;background-repeat: no-repeat;">
+          </div>
+        </div>
+      </div>
+    </div>`;
+  } else {
+  document.querySelector("#chat-log").innerHTML += `
+        <div class="col-4 mt-3 mb-1" style="background-color: rgba(0, 0, 0, 0);">
+            <div class="card" style="background-color: rgba(0, 0, 0, 0); border-color: rgba(0, 0, 0, 0);">
+                <div style="padding-top: 1.25rem; background-image: url(${otherChatTopUrl}); background-size: 100%, 100%; background-repeat: no-repeat;">
+                </div>
+          <div style="flex: 1 1 auto; background-image: url(${otherChatMidUrl}); background-size: 100%, 100%; padding: 0 1.25rem 1rem 1.25rem;">
+            <h5 class="card-title">${author}</h5>
+            ${message}
+          </div>
+          <div class="card-body" style="background-image: url(${otherChatBotUrl});background-size: 100%, 100%;background-repeat: no-repeat;">
+          </div>
+        </div>
+      </div>`;
+  }
 };
 
 // WebSocket 연결
